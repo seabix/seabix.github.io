6c6d8863c6d8860180ee968239c5c72b8a39e721
Benoît <37414714+seabix@users.noreply.github.com>
2019-05-06 16:36:48 -0400

    Add files via upload

---
+++ w200-config.js
@@ -0,0 +1,20 @@
+/* ************* EACH TERM EDIT ZOOM SECTION LINKS ********************* */
+var section1ZoomLinks = ["179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279","179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279"];
+var section2ZoomLinks = ["179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279","179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279"];
+var section3ZoomLinks = ["179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279","179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279"];
+var section4ZoomLinks = ["179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279","179123279", "179123279", "179123279", "179123279", "179123279", "179123279", "179123279"];
+
+/* ************** EACH TERM: CHANGE EXAM DATES - AUTO ACTIVATE LINK **** */
+var exam1Date = "2019-06-25";	/* in form YYYY-MM-DD */
+var exam2Date = "2019-07-20";
+
+/* ************* EACH TERM EDIT FOR WEEK DATES ************************* */
+var datesOfWeek = ["May 7-11", "May 14-18", "May 21-25", "May 28-Jun 1","Jun 4-8", "Jun 11-15", "Jun 18-22", "Jun 25-29","Jul 2-6", "Jul 9-13", "Jul 16-20", "Jul 23-27", "Jul 20-Aug 3", "Aug 6-10"];
+
+/* ************* EACH TERM EDIT FOR WEEKLY SLIDE SETS ******************* */
+var sessionsLinkBase = "https://github.com/UCB-INFO-PYTHON/assignments_upstream_summer19/tree/master/";
+var sessionLinks = [
+	"week_01", "week_02", "week_03", "week_04", "week_05", "week_06", "week_07", "week_08", "week_09", "week_10", "week_11", "week_12", "week_13", "week_14"];
+
+/* ************* COMMUNICATION OPTIONS: SLACK ************************* */
+var slackLink = "https://ucbischool.slack.com/messages/G4FAZ2DPA/";
